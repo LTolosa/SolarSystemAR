0f72ec384e2e3186611f5affc3363e377dc38371
LTolosa <let2120@columbia.edu>
2015-05-21 16:37:44 -0400

    Testing orbits on Earth. Remove trail renderer.

--- SolarSystemAR/Assets/Scripts/OrbitPlanet.cs
+++ SolarSystemAR/Assets/Scripts/OrbitPlanet.cs
@@ -16,10 +16,12 @@ public class OrbitPlanet : MonoBehaviour {
 
     const float earthOrbit = 365.256363004f;    //Number of days for earth's orbit
     GameObject planetOrbit;
+    LineRenderer trail;
 
 	// Use this for initialization
 	void Start () {
         angle = (float) Random.Range(0,  2*Mathf.PI);
+        //if (name == "Earth") angle = 0;
 
         //Angular speed with earth normalization
         //Makes it so 1 earth orbit = 1 year
@@ -33,28 +35,37 @@ public class OrbitPlanet : MonoBehaviour {
                           + Mathf.Pow(transform.localPosition.z, 2));
         worldRadius = Mathf.Sqrt(Mathf.Pow(transform.position.x, 2)
                                + Mathf.Pow(transform.position.z, 2));
-
+        Matrix4x4 mat = this.transform.worldToLocalMatrix;
+        
+        //Testing out new orbit structure with earth
         /*
         if (name == "Earth")
         {
             planetOrbit = this.transform.FindChild("EarthOrbit").gameObject;
-            LineRenderer trail = planetOrbit.GetComponent<LineRenderer>();
+            trail = planetOrbit.GetComponent<LineRenderer>();
+            trail.useWorldSpace = false;
             trail.SetVertexCount(41);
             float angDif = 2*Mathf.PI / 40f;
             int j = 0;
-            for (float i = 0; i <= 2*Mathf.PI + 2*Mathf.PI/40f; i += 2*Mathf.PI/40f)
+            for (float i = 0; i <= 2*Mathf.PI + 2*Mathf.PI/40f; i += angDif)
             {
-                trail.SetPosition(j++, new Vector3(worldRadius * Mathf.Cos(i),
+                Vector3 v =  new Vector3(worldRadius * Mathf.Cos(i),
                                                    this.transform.position.y,
-                                                   worldRadius * Mathf.Sin(i)));
+                                                   worldRadius * Mathf.Sin(i));
+                Debug.Log("Vector " + v);
+                Debug.Log("Matrix " + mat);
+                Debug.Log("After Matrix Vector " + this.transform.InverseTransformPoint(this.transform.position - v));
+                trail.SetPosition(j++, this.transform.InverseTransformPoint(v));
             }
 
         }
-         */
+        */
      
 
 	}
 	
+
+    
 	// Update is called once per frame
 	void Update () {
         if (orbit)
@@ -67,10 +78,24 @@ public class OrbitPlanet : MonoBehaviour {
             transform.localPosition = new Vector3(radius * Mathf.Cos(angle), 
                                                   transform.localPosition.y, 
                                                   radius * Mathf.Sin(angle));
+            if (name == "Earth")
+            {
+                this.transform.FindChild("EarthOrbit").position = new Vector3(0, 0, 0);
+            }
+
         }
 
         if (angle > 2 * Mathf.PI) angle = angle - 2*Mathf.PI;
         if (angle < 0) angle = angle + 2 * Mathf.PI;
+        /*
+        if (name == "Earth")
+        {
+            for (int i = 0; i < 40; i++)
+            {
+
+            }
+        }
+         */
         /*
         posdiff = newpos - oldpos;
         for (if=1;if<posarray.size; ++if)
